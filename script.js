75734c6d2b0efe180baded51f0cad6d2ade72a99
crackheadakira <lasn.mine@gmail.com>
2022-07-29 16:40:47 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'dark-theme') {
+        setTheme('light-theme');
+    } else {
+        setTheme('dark-theme');
+    }
+}
+
+(function () {
+    if (localStorage.getItem('theme') === 'dark-theme') {
+        setTheme('dark-theme');
+        document.getElementById('slider').checked = false;
+    } else {
+        setTheme('light-theme');
+        document.getElementById('slider').checked = true;
+    }
+})();
