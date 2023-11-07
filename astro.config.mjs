ccd6b42f5d10267e0053e05bb7a9ab828b6f3436
sayykii <sayykiipersonal@gmail.com>
2023-11-07 23:15:07 +0200

    chore: package.json update #1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import vue from "@astrojs/vue";
@@ -6,7 +6,9 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
-  server: {
-    port: import.meta.env.PORT || 3000,
+  vite: {
+    define: {
+      "process.env": {},
+    },
   }
 });
