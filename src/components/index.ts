1c152632547f34cd9223816ea25e3180867290b0
sayykii <sayykiipersonal@gmail.com>
2024-01-06 11:20:37 +0200

    chore: add package-lock

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
-export { default as Header } from './Header.astro';
-export { default as VueCard } from './VueCard.vue';
 export { default as Footer } from './Footer.astro';
+export { default as Header } from './Header.astro';
 export { default as InviteButton } from './InviteButton.vue';
